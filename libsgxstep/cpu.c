96f20e3a8b1fce212da58380388dcbe934357f07
Jo Van Bulck <jo.vanbulck@cs.kuleuven.be>
2018-03-05 17:57:15 +0100

    Add RDTSCP libsgxstep support.

---
+++ libsgxstep/cpu.c
@@ -0,0 +1,63 @@
+/*
+ *  This file is part of the SGX-Step enclave execution control framework.
+ *
+ *  Copyright (C) 2017 Jo Van Bulck <jo.vanbulck@cs.kuleuven.be>,
+ *                     Raoul Strackx <raoul.strackx@cs.kuleuven.be>
+ *
+ *  SGX-Step is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  SGX-Step is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with SGX-Step. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "cpu.h"
+
+/*
+ * Code adapted from
+ * https://github.com/IAIK/flush_flush/blob/master/sc/cacheutils.h
+ */
+uint64_t rdtsc_begin( void )
+{
+  uint64_t a, d;
+
+  asm volatile (
+    "mfence\n\t"
+    "CPUID\n\t"
+    "RDTSCP\n\t"
+    "mov %%rdx, %0\n\t"
+    "mov %%rax, %1\n\t"
+    "mfence\n\t"
+    : "=r" (d), "=r" (a)
+    :
+    : "%rax", "%rbx", "%rcx", "%rdx");
+
+  a = (d<<32) | a;
+  return a;
+}
+
+uint64_t rdtsc_end( void )
+{
+  uint64_t a, d;
+
+  asm volatile(
+    "mfence\n\t"
+    "RDTSCP\n\t"
+    "mov %%rdx, %0\n\t"
+    "mov %%rax, %1\n\t"
+    "CPUID\n\t"
+    "mfence\n\t"
+    : "=r" (d), "=r" (a)
+    :
+    : "%rax", "%rbx", "%rcx", "%rdx");
+
+  a = (d<<32) | a;
+  return a;
+}
