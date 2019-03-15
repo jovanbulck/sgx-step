de44cb4de4936f1354d1d6ac734773e6dd9e2784
Jo Van Bulck <jo.vanbulck@cs.kuleuven.be>
2019-03-15 16:10:48 +0100

    Rebase patches to upstream linux-sgx v2.4.
    Linux 2.4 Open Source Gold Release

--- README.md
+++ README.md
@@ -123,7 +123,7 @@ timer interval (`libsgxstep/config.h`).
 
 To enable easy registration of a custom Asynchronous Exit Pointer (AEP) stub,
 we modified the untrusted runtime of the official Intel SGX SDK. Proceed as
-follows to checkout [linux-sgx](https://github.com/01org/linux-sgx) v2.2 and
+follows to checkout [linux-sgx](https://github.com/01org/linux-sgx) v2.4 and
 apply our patches.
 
 ```bash
--- patch_sdk.sh
+++ patch_sdk.sh
@@ -8,11 +8,5 @@ then
     patch -p1 < ../0000-32bit-compatibility-fixes.patch
 fi
 
-echo "=== patching AEP   ==="
-patch -p1 < ../0001-Support-to-reconfigure-Asynchronous-Exit-Pointer-AEP.patch
-
-echo "=== patching TCS   ==="
-patch -p1 < ../0002-Add-support-to-retrieve-most-recently-used-TCS-point.patch
-
-echo "=== patching EBASE ==="
-patch -p1 < ../0003-Support-to-set-enclave-load-location.patch
+echo "=== patching AEP/TCS/EBASE ==="
+patch -p1 < ../0001-reconfigure-AEP-TCS-ebase.patch
